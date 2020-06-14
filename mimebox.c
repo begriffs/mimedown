65aad76ed900111262b1e1ff1ebd3703adef2e04
Joe Nelson <joe@begriffs.com>
2020-06-14 12:56:37 -0500

    Print debug log of markdown tree

--- mimebox.c
+++ mimebox.c
@@ -7,6 +7,7 @@ int main(void)
 {
 	char buf[BUFSIZ];
 	size_t bytes;
+	int depth = 0;
 	cmark_node *doc;
 
 	cmark_parser *p = cmark_parser_new(CMARK_OPT_DEFAULT);
@@ -18,6 +19,40 @@ int main(void)
 	}
 	doc = cmark_parser_finish(p);
 	cmark_parser_free(p);
+
+	cmark_event_type ev_type;
+	cmark_iter *iter = cmark_iter_new(doc);
+
+	while ((ev_type = cmark_iter_next(iter)) != CMARK_EVENT_DONE)
+	{
+		cmark_node *cur = cmark_iter_get_node(iter);
+		cmark_node_type type = cmark_node_get_type(cur);
+		for(int i = 0; i < depth; i++)
+			putchar(' ');
+		switch (ev_type)
+		{
+			case CMARK_EVENT_NONE:
+				printf("?");
+				break;
+			case CMARK_EVENT_DONE:
+				printf(".");
+				break;
+			case CMARK_EVENT_ENTER:
+				printf(">");
+				if (type != CMARK_NODE_TEXT && type != CMARK_NODE_CODE_BLOCK &&
+						type != CMARK_NODE_SOFTBREAK)
+					depth++;
+				break;
+			case CMARK_EVENT_EXIT:
+				printf("<");
+				depth--;
+				break;
+			default:
+				abort();
+		}
+		printf(" %s\n", cmark_node_get_type_string(cur));
+	}
+	cmark_iter_free(iter);
 
 	return EXIT_SUCCESS;
 }
