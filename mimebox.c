79dd93d8dc6c7618cb1dfd9ad8a58b2643d6eff4
Joe Nelson <joe@begriffs.com>
2020-06-14 13:04:05 -0500

    Crudely separate codeblocks into attachments

--- mimebox.c
+++ mimebox.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* libcmark */
 #include <cmark.h>
 
 int main(void)
 {
 	char buf[BUFSIZ];
 	size_t bytes;
-	int depth = 0;
 	cmark_node *doc;
 
 	cmark_parser *p = cmark_parser_new(CMARK_OPT_DEFAULT);
@@ -23,34 +23,44 @@ int main(void)
 	cmark_event_type ev_type;
 	cmark_iter *iter = cmark_iter_new(doc);
 
+	enum section_type
+	{
+		SEC_NONE, SEC_MSG, SEC_CODE
+	} section, prev_section = SEC_NONE;
+
+	puts("MIME-Version: 1.0\n"
+	     "Content-Type: multipart/mixed; boundary=boundary42");
+
 	while ((ev_type = cmark_iter_next(iter)) != CMARK_EVENT_DONE)
 	{
+		int nth_code_block = 0;
 		cmark_node *cur = cmark_iter_get_node(iter);
 		cmark_node_type type = cmark_node_get_type(cur);
-		for(int i = 0; i < depth; i++)
-			putchar(' ');
-		switch (ev_type)
+		if (ev_type == CMARK_EVENT_ENTER)
 		{
-			case CMARK_EVENT_NONE:
-				printf("?");
-				break;
-			case CMARK_EVENT_DONE:
-				printf(".");
-				break;
-			case CMARK_EVENT_ENTER:
-				printf(">");
-				if (type != CMARK_NODE_TEXT && type != CMARK_NODE_CODE_BLOCK &&
-						type != CMARK_NODE_SOFTBREAK)
-					depth++;
-				break;
-			case CMARK_EVENT_EXIT:
-				printf("<");
-				depth--;
-				break;
-			default:
-				abort();
+			section = (type == CMARK_NODE_CODE_BLOCK)
+				? SEC_CODE
+				: SEC_MSG;
+			if (section != prev_section)
+			{
+				prev_section = section;
+				puts("\n--boundary42");
+				if (section == SEC_CODE)
+				{
+					puts("Content-Type: text/x-c");
+					printf("Content-Disposition: inline; filename=%d.c;\n\n",
+					       nth_code_block++);
+				}
+				else
+				{
+					puts("Content-Type: text/plain");
+					puts("Content-Disposition: inline\n");
+				}
+			}
+			const char *content = cmark_node_get_literal(cur);
+			if (content)
+				fputs(content, stdout);
 		}
-		printf(" %s\n", cmark_node_get_type_string(cur));
 	}
 	cmark_iter_free(iter);
 
